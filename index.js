526fbbae9cb035a96d03aa15e95d93d81ec69056
James Capadocia <james.capadocia@gmail.com>
2021-09-22 00:34:28 -0500

    Created inquirer prompts that work when 'node index.js' is fired in the command line.

--- index.js
+++ index.js
@@ -3,7 +3,36 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?",
+    },
+    {
+        type: "input",
+        name: "aboutMyApp",
+        message: "Provide the description, installation instructions, usage information, contribution guidelines, and test instructions.",  
+    },
+    {
+        type: "list",
+        name: "LinkedIn",
+        choices: ["MIT", "Other"],  
+    },
+    {
+        type: "input",
+        name: "GitHub",
+        message: "What is your GitHub username?",  
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",  
+    },
+    
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
